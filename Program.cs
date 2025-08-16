e4692d6c0675becac02803b6b9dc5e687b47ba72
Robin <lassak.robin@gmail.com>
2025-08-16 20:36:26 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MailPullerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my PullerApp");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
