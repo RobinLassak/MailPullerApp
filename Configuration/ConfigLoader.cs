789aeda606c2f1897613502a04a3a01da35e4c21
Robin <lassak.robin@gmail.com>
2025-08-17 20:46:42 +0200

    nacteni konfigurace z json, namapovani na AppConfig + test

---
+++ Configuration/ConfigLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailPullerApp.Configuration
+{
+    internal class ConfigLoader
+    {
+        public static AppConfig Load(string filePath = "appsettings.json")
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("File path cannot be null or empty.", nameof(filePath));
+            }
+            try
+            {
+                var config = System.Text.Json.JsonSerializer.Deserialize<AppConfig>(System.IO.File.ReadAllText(filePath));
+                if (config == null)
+                {
+                    throw new InvalidOperationException("Configuration could not be loaded from the specified file.");
+                }
+                return config;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to load configuration from {filePath}.", ex);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@
         {
             Console.WriteLine("Welcome to my PullerApp");
             Console.WriteLine("Press any key to continue");
+            try
+            {
+                var config = Configuration.ConfigLoader.Load();
+                Console.WriteLine("Configuration loaded successfully.");
+                Console.WriteLine($"Email: {config.Logging.Log4NetConfigFile}");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine($"An error occurred while loading the configuration: {e.Message}");
+            }
             Console.ReadKey();
         }
     }
