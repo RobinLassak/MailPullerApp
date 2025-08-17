03394be0ffb407490280009e5fdb88a1633abf97
Robin <lassak.robin@gmail.com>
2025-08-17 19:04:11 +0200

    Hlavni konfiguracni soubor

---
+++ AppConfig.cs
@@ -0,0 +1,19 @@
+﻿using MailPullerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailPullerApp
+{
+    internal class AppConfig
+    {
+        public GraphSettings Graph { get; set; } = default!;
+        public MailboxSettings Mailbox { get; set; } = default!;
+        public OutputSettings Output { get; set; } = default!;
+        public StateSettings State { get; set; } = default!;
+        public SecuritySettings Security { get; set; } = default!;
+        public LoggingSettings Logging { get; set; } = default!;
+    }
+}
