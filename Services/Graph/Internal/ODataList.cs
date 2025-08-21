16c1439051594db184eaf2195fab79c4f6fd7873
Robin <lassak.robin@gmail.com>
2025-08-21 07:35:51 +0200

    strankovani a ukladani stavu

--- Services/Graph/Internal/ODataList.cs
+++ Services/Graph/Internal/ODataList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MailPullerApp.Services.Graph.Internal
@@ -10,5 +11,40 @@ namespace MailPullerApp.Services.Graph.Internal
     // Obsahuje aktuální stránku položek a další metadata
     internal class ODataList<T>
     {
+        [JsonPropertyName("@odata.context")]
+        public string? ODataContext { get; set; }
+
+        [JsonPropertyName("@odata.nextLink")]
+        public string? ODataNextLink { get; set; }
+
+        [JsonPropertyName("@odata.deltaLink")]
+        public string? ODataDeltaLink { get; set; }
+
+        [JsonPropertyName("value")]
+        public List<T> Value { get; set; } = new List<T>();
+
+        // Pomocné vlastnosti pro práci se stránkováním
+        public bool HasNextPage => !string.IsNullOrEmpty(ODataNextLink);
+        public bool HasDeltaLink => !string.IsNullOrEmpty(ODataDeltaLink);
+        public int Count => Value?.Count ?? 0;
+        public bool IsEmpty => Count == 0;
+
+        // Metoda pro získání další stránky
+        public string? GetNextPageUrl() => ODataNextLink;
+
+        // Metoda pro získání delta linku (pro delta sync)
+        public string? GetDeltaLink() => ODataDeltaLink;
+
+        // Metoda pro vytvoření prázdného seznamu
+        public static ODataList<T> CreateEmpty()
+        {
+            return new ODataList<T>
+            {
+                Value = new List<T>(),
+                ODataContext = null,
+                ODataNextLink = null,
+                ODataDeltaLink = null
+            };
+        }
     }
 }
